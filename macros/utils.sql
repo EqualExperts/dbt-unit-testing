d4e6b7cc179a9dc8f6d40d1895d1e8d20e40e106
Cláudio Filipe Paiva Diniz <claudio.diniz@equalexperts.com>
2022-05-19 09:24:47 +0100

    Refactor macro organisation (#71)

--- macros/utils.sql
+++ macros/utils.sql
@@ -99,3 +99,31 @@
   {% set pure = mocking_strategy | upper == 'PURE' %}
   {{ return ({"full": full, "simplified": simplified, "database": database, "pure": pure}) }}
 {% endmacro %}
+
+{% macro quote_identifier(identifier) %}
+    {{ return(adapter.dispatch('quote_identifier','dbt_unit_testing')(identifier)) }}
+{% endmacro %}
+
+{% macro default__quote_identifier(identifier) -%}
+    {% if identifier.startswith('"') %}
+      {{ return(identifier) }}
+    {% else %}
+      {{ return('"' ~ identifier ~ '"') }}
+    {% endif %}
+{%- endmacro %}
+
+{% macro bigquery__quote_identifier(identifier) %}
+    {% if identifier.startswith('`') %}
+      {{ return(identifier) }}
+    {% else %}
+      {{ return('`' ~ identifier ~ '`') }}
+    {% endif %}
+{% endmacro %}
+
+{% macro snowflake__quote_identifier(identifier) %}
+    {% if identifier.startswith('"') %}
+      {{ return(identifier) }}
+    {% else %}
+      {{ return('"' ~ identifier | upper ~ '"') }}
+    {% endif %}
+{% endmacro %}
