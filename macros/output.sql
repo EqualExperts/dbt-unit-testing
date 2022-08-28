d5060bb7e9e325f9c87e8904a8a4717d6a4713b5
Pedro Sousa <pedronsousa@gmail.com>
2022-08-28 09:55:23 +0100

    show only columns with differences

--- macros/output.sql
+++ macros/output.sql
@@ -1,39 +1,59 @@
 {% macro print_table(agate_table) %}
+  {% set columns_start_index = 2 %}
   {% set columns_info = [] %}
   {% for col_name in agate_table.column_names %}
-    {% set column_info = {"max_length": col_name | length, "is_string": false} %}
+    {% set col_index = loop.index0 %}
+    {% set column_info = {"max_length": col_name | length, "is_string": false, "has_differences": false} %}
     {% for row in agate_table.rows %}
       {% set cell_value = row[col_name] %}
       {% set l = cell_value | string | length %}
       {% if l > column_info.max_length %}
         {% do column_info.update({"max_length": l}) %}
       {% endif %}
-    {% do column_info.update({"is_string": column_info.is_string or cell_value is string}) %}
-    {% endfor %}
+      {% do column_info.update({"is_string": column_info.is_string or cell_value is string}) %}
+
+      {% if col_index >= columns_start_index %}
+        {% if column_info.previous_value is defined and column_info.previous_value != cell_value %}
+          {% do column_info.update({"has_differences": true}) %}
+        {% endif %}
+        {% do column_info.update({"previous_value": cell_value}) %}
+      {% endif %}
+
+    {% endfor %}  
     {% set columns_info = columns_info.append(column_info) %}
   {% endfor %}
 
   {% set cells = [] %}
   {% for col_name in agate_table.column_names %}
-    {% set padded = dbt_unit_testing.pad(col_name, columns_info[loop.index0].max_length, pad_right=columns_info[loop.index0].is_string) %}
-    {% do cells.append(padded) %}
+    {% set col_index = loop.index0 %}
+    {% if col_index < columns_start_index or columns_info[col_index].has_differences %}
+      {% set padded = dbt_unit_testing.pad(col_name, columns_info[col_index].max_length, pad_right=columns_info[col_index].is_string) %}
+      {% do cells.append(padded) %}
+    {% endif %}
   {% endfor %}
-  {% do log("| " ~ cells | join(" | ") ~ " |", info=True) %}
+  {{ dbt_unit_testing.println("| " ~ cells | join(" | ") ~ " |")}}
 
   {% set cells = [] %}
   {% for col_name in agate_table.column_names %}
-    {% do cells.append(dbt_unit_testing.pad("", columns_info[loop.index0].max_length, c="-")) %}
+    {% set col_index = loop.index0 %}
+    {% if col_index < columns_start_index or columns_info[col_index].has_differences %}
+      {% do cells.append(dbt_unit_testing.pad("", columns_info[col_index].max_length, c="-")) %}
+    {% endif %}
   {% endfor %}
-  {% do log("| " ~ cells | join(" | ") ~ " |", info=True) %}
+  {{ dbt_unit_testing.println("| " ~ cells | join(" | ") ~ " |")}}
 
   {% for row in agate_table.rows %}
     {% set cells = [] %}
     {% for cell_value in row %}
-      {% set padded = dbt_unit_testing.pad(cell_value, columns_info[loop.index0].max_length, pad_right=cell_value is string) %}
-      {% do cells.append(padded) %}
+      {% set col_index = loop.index0 %}
+      {% if col_index < columns_start_index or columns_info[col_index].has_differences %}
+        {% set padded = dbt_unit_testing.pad(cell_value, columns_info[col_index].max_length, pad_right=cell_value is string) %}
+        {% do cells.append(padded) %}
+      {% endif %}
     {% endfor %}
-    {% do log("| " ~ cells | join(" | ") ~ " |", info=True) %}
+    {{ dbt_unit_testing.println("| " ~ cells | join(" | ") ~ " |")}}
   {% endfor %}
+
 {% endmacro %}
 
 {% macro pad(v, pad, pad_right=false, c=" ") %}
