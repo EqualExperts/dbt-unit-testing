7682bf804fefa4664abbe2c98fae0ab865b9f98c
Claudio Diniz <claudio.diniz.equalexperts@autodoc.eu>
2023-03-18 09:41:35 +0000

    Update dbt versions to fix pytz bug

--- run-all-integration-tests.sh
+++ run-all-integration-tests.sh
@@ -9,5 +9,5 @@ fi
 
 PROFILE="$1"
 
-./run-tests-on-dbt-version.sh "run-integration-tests" "$PROFILE" "1.3.0"
-./run-tests-on-dbt-version.sh "run-integration-tests" "$PROFILE" "1.4.0"
+./run-tests-on-dbt-version.sh "run-integration-tests" "$PROFILE" "1.3.3"
+./run-tests-on-dbt-version.sh "run-integration-tests" "$PROFILE" "1.4.5"
