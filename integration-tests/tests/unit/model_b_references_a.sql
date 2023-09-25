7b492cedb499dd246ea1aa621c0d6f4b2fd663ab
Pedro Sousa <pedronsousa@gmail.com>
2023-09-25 18:27:59 +0100

    Add test for cte quoting

--- integration-tests/tests/unit/model_b_references_a.sql
+++ integration-tests/tests/unit/model_b_references_a.sql
@@ -14,3 +14,15 @@
     select 1 as a, 'b' as b
   {% endcall %}
 {% endcall %}
+
+UNION ALL
+
+{% call test_condition_on_model_query('model_b_references_a', "should ", {}, 
+                                       assert_should_contain, dbt_unit_testing.quote_identifier("model_a")) %}
+  {% call dbt_unit_testing.mock_ref ('model_a') %}
+    select 1 as id
+  {% endcall %}
+  {% call dbt_unit_testing.expect() %}
+    select 1 as id
+  {% endcall %}
+{% endcall %}
